7496e59f4c60278488182e8b3a2af731d2ff8e04
Bvernimm <bazvernim@outlook.fr>
2022-05-10 15:17:33 +0200

    move

--- src/sort_100.c
+++ src/sort_100.c
@@ -6,7 +6,7 @@
 /*   By: bvernimm <bvernimm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 12:05:47 by bvernimm          #+#    #+#             */
-/*   Updated: 2022/05/10 15:14:18 by bvernimm         ###   ########.fr       */
+/*   Updated: 2022/05/10 15:17:22 by bvernimm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,35 +70,34 @@ void	modify_move(t_cost **move, int r, int place, int cost)
 	}
 }
 
-void	sort_100(t_stack **stack_a, t_stack **stack_b, int len)
+void	sort_100(t_stack **a, t_stack **b, int len)
 {
 	int		i;
-	t_stack	*tmp;
 	t_cost	*move;
 
 	move = malloc(sizeof(t_cost));
-	tmp = (*stack_a);
-	printf("pushing %d on %d\n", tmp->value, (*stack_b)->value);
-	move->rb_cost = calculate_r_cost(tmp->value, stack_b);
-	move->rrb_cost = calculate_rr_cost(tmp->value, stack_b);
+	printf("pushing %d on %d\n", (*a)->value, (*b)->value);
+	//modify_move(&move, 1, 0, calculate_r_cost(tmp->value, stack_b));
+	/*move->rb_cost = calculate_r_cost((*a)->value, b);
+	move->rrb_cost = calculate_rr_cost((*a)->value, b);
 	move->r_place = 0;
-	move->rr_place = 0;
+	move->rr_place = 0;*/
 	i = 0;
 	while (i < len - 1)
 	{
-		if (move->rb_cost > calculate_r_cost(tmp->value, stack_b) + i)
+		if (move->rb_cost > calculate_r_cost((*a)->value, b) + i)
 		{
-			move->rb_cost = calculate_r_cost(tmp->value, stack_b) + i;
+			move->rb_cost = calculate_r_cost((*a)->value, b) + i;
 			move->r_place = i;
 		}
-		if (move->rrb_cost > calculate_rr_cost(tmp->value, stack_b) + i)
+		if (move->rrb_cost > calculate_rr_cost((*a)->value, b) + i)
 		{
-			move->rrb_cost = calculate_rr_cost(tmp->value, stack_b) + i;
+			move->rrb_cost = calculate_rr_cost((*a)->value, b) + i;
 			move->rr_place = i;
 		}
 		i++;
-		if (tmp->next)
-			tmp = tmp->next;
+		if ((*a)->next)
+			(*a) = (*a)->next;
 	}
 	printf("r cost : %d, place %d\nrr cost : %d, place %d\n", move->rb_cost, move->r_place, move->rrb_cost, move->rr_place);
 }
