2ec903eb01cafba0e414540c7ee52a3f5f99e155
Bvernimm <bazvernim@outlook.fr>
2022-05-10 15:12:49 +0200

    move

--- src/sort_100.c
+++ src/sort_100.c
@@ -6,7 +6,7 @@
 /*   By: bvernimm <bvernimm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 12:05:47 by bvernimm          #+#    #+#             */
-/*   Updated: 2022/05/10 13:37:35 by bvernimm         ###   ########.fr       */
+/*   Updated: 2022/05/10 15:12:02 by bvernimm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,14 +25,8 @@ int	calculate_rr_cost(int nb, t_stack **stack_b)
 		tmp = tmp->next;
 	while (tmp->previous)
 	{
-		//printf("%d > %d && %d < %d ?\n", tmp->value, nb, tmp->previous->value, nb);
 		if (tmp->value > nb && tmp->previous->value < nb)
-		{
-			//printf("%d > %d && %d < %d !\n", tmp->value, nb, tmp->previous->value, nb);
 			break ;
-		}
-		//else
-		//	printf("Nop\n");
 		rr_cost++;
 		tmp = tmp->previous;
 	}
@@ -52,14 +46,8 @@ int	calculate_r_cost(int nb, t_stack **stack_b)
 	tmp = (*stack_b);
 	while (tmp->next)
 	{
-		//printf("%d < %d && %d > %d ?\n", tmp->value, nb, tmp->next->value, nb);
 		if (tmp->value < nb && tmp->next->value > nb)
-		{
-		//	printf("%d < %d && %d > %d !\n", tmp->value, nb, tmp->next->value, nb);
 			break ;
-		}
-		//else
-		//	printf("Nop\n");
 		r_cost++;
 		tmp = tmp->next;
 	}
@@ -72,31 +60,31 @@ void	sort_100(t_stack **stack_a, t_stack **stack_b, int len)
 {
 	int		i;
 	t_stack	*tmp;
-	t_cost	*cost;
+	t_cost	*move;
 
-	cost = malloc(sizeof(t_cost));
+	move = malloc(sizeof(t_cost));
 	tmp = (*stack_a);
 	printf("pushing %d on %d\n", tmp->value, (*stack_b)->value);
-	cost->r_cost = calculate_r_cost(tmp->value, stack_b);
-	cost->rr_cost = calculate_rr_cost(tmp->value, stack_b);
-	cost->r_place = 0;
-	cost->rr_place = 0;
+	move->r_cost = calculate_r_cost(tmp->value, stack_b);
+	move->rr_cost = calculate_rr_cost(tmp->value, stack_b);
+	move->r_place = 0;
+	move->rr_place = 0;
 	i = 0;
 	while (i < len - 1)
 	{
-		if (cost->r_cost > calculate_r_cost(tmp->value, stack_b) + i)
+		if (move->r_cost > calculate_r_cost(tmp->value, stack_b) + i)
 		{
-			cost->r_cost = calculate_r_cost(tmp->value, stack_b) + i;
-			cost->r_place = i;
+			move->r_cost = calculate_r_cost(tmp->value, stack_b) + i;
+			move->r_place = i;
 		}
-		if (cost->rr_cost > calculate_rr_cost(tmp->value, stack_b) + i)
+		if (move->rr_cost > calculate_rr_cost(tmp->value, stack_b) + i)
 		{
-			cost->rr_cost = calculate_rr_cost(tmp->value, stack_b) + i;
-			cost->rr_place = i;
+			move->rr_cost = calculate_rr_cost(tmp->value, stack_b) + i;
+			move->rr_place = i;
 		}
 		i++;
 		if (tmp->next)
 			tmp = tmp->next;
 	}
-	printf("r cost : %d, place %d\nrr cost : %d, place %d\n", cost->r_cost, cost->r_place, cost->rr_cost, cost->rr_place);
+	printf("r cost : %d, place %d\nrr cost : %d, place %d\n", move->r_cost, move->r_place, move->rr_cost, move->rr_place);
 }
